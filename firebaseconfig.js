7067a4e8f7074b8ba6b4070d0311ad9eaac3533c
raulereno <raulereno@gmail.com>
2023-02-06 09:43:16 -0300

    agregando firebase config

---
+++ firebaseconfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyCqbipD7_W_9BCkGBX5HCLvQjMFK7efvGc",
+  authDomain: "cordoba-hacker-space.firebaseapp.com",
+  projectId: "cordoba-hacker-space",
+  storageBucket: "cordoba-hacker-space.appspot.com",
+  messagingSenderId: "149939850949",
+  appId: "1:149939850949:web:08031c222b12934494eaac",
+  measurementId: "G-1JP6N5F769",
+};
